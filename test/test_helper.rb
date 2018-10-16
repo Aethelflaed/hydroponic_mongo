95b66e968757901bb250b89d58dc9a801acf18c8
Geoffroy Planquart <geoffroy@planquart.fr>
2018-10-16 15:41:25 +0200

    added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+require 'bundler/setup'
+require 'simplecov'
+SimpleCov.configure do
+  add_filter '/test/'
+end
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "hydroponic_mongo"
 
