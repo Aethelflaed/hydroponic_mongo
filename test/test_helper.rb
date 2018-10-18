787b144dc792909f70ec3d7879f7795df93801c3
Geoffroy Planquart <geoffroy@planquart.fr>
2018-10-18 14:16:22 +0200

    configure mongo logger output for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
 require 'bundler/setup'
+
+require 'pry'
 require 'simplecov'
 SimpleCov.configure do
   add_filter '/test/'
@@ -7,5 +9,9 @@ SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "hydroponic_mongo"
+
+Mongo::Logger.logger = Logger.new(File.open(File.expand_path('../../log/test.log', __FILE__), 'a+'))
+
+Mongo.hydroponic!
 
 require "minitest/autorun"
